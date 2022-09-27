0ed9332b648bf2fd3774f3aaf0e3ca1bed618fee
lalitsankar96 <63387818+lalitsankar96@users.noreply.github.com>
2022-09-27 12:02:42 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,7 @@
 class Hello{
     public static void main(String[] args) {
         System.out.println("hello lalit");
+        System.out.println("welcome to GITHUB");
         System.out.println("welcome to practice");
     }
 }
